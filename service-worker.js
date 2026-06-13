ac5930b3966109925b56d0486632c5782c172681
joedurufour-cmyk <joedurufour@gmail.com>
2026-06-13 10:16:01 -0500

    feat: add service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+const CACHE_NAME = "oi-os-v1";
+const ASSETS = ["./", "./index.html", "./style.css", "./manifest.json", "./icon.svg"];
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
